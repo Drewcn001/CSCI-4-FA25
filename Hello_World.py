9270883580f69d484f2c7db5aadc447234d221f4
Drewcn001 <dnickell001@student.butte.edu>
2025-09-24 23:54:17 +0000

    trying to commit

---
+++ Hello_World.py
@@ -0,0 +1,18 @@
+H = "Hello"
+W = "World"
+print (H,W)
+import array as Arr
+A = Arr.array('l',[1,2,3,4])
+
+
+P = list([246,345])
+L = list ('246345')
+print (P)
+print (L)
+T = tuple(A)
+print(T)
+R = list(range(1,10,5))
+print(R)
+print (list(A))
+P.reverse()
+print(P)
