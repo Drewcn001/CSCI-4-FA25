3a7ce23d7b03cf2c1401abfdd81abd34ff5bdaec
Drewcn001 <dnickell001@student.butte.edu>
2025-10-30 17:12:13 +0000

    Working on Queue and Stack

--- Practice.py
+++ Practice.py
@@ -1,15 +1,55 @@
 # queue 
 
 # are linear structures
-q.Queue = q
-
 q = []
-# push things into them
-q.append('A')
-q.append('B')
-q.append('C')
-# queue pop things off
-q.pop()
+
+head = 0
+
+def tail(q):
+    return len(q)
+
+def push(q, e):
+    q.append(e)
+    return q
+
+def pop(q):
+    return q.pop(head)
+
+push(q, 4)
+push(q, 3)
+push(q, 5)
+pop(q)
+
+print (len(q))
+print (q)
 
 
-print("Queue: ", queue)
+
+
+
+s = []
+
+head = 0
+
+def push(s, g):
+    s.insert(0, g)
+    return g
+
+def pop(s):
+    return s.pop(head)
+
+push(s, 1)
+push(s, 2)
+push(s, 3)
+
+print (s)
+pop (s)
+print (s)
+
+push(s,9)
+push(s,8)
+push(s,7)
+
+print (s)
+pop (s)
+print (s)
