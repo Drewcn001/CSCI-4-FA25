4ad8943cf35b8fc1893c4241c8190d16ac9d1de4
Drewcn001 <dnickell001@student.butte.edu>
2025-11-25 18:52:00 +0000

    euclidean work

---
+++ Euclid.py
@@ -0,0 +1,11 @@
+x = input("enter x cordinate: ")
+y = input("enter y cordinate: ")
+dict_point = [x,y]
+x2 = input("enter 2nd x cordinate: ")
+y2 = input("enter 2nd y cordinate: ")
+
+g = [x2,y2]
+
+def euclid_distance (,): 
+    return (h[0]) - (g[0])
+print (euclid_distance)
